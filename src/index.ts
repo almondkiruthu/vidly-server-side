c981f4063f41cb6cf2c5dac2dcc062cb9d143937
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-03-12 01:41:19 +0300

    feat: add put and delete methods

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,12 @@ const genres = [
   { id: 3, name: 'Romance' },
 ];
 
-app.get('/api/genres', (req, res) => {
+app.get('/api/genres', (_req, res) => {
   res.send(genres);
 });
 
 app.get('/api/genres/:id', (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
   if (!genre)
     return res.status(404).send('The genre with the given ID was not found.');
   res.send(genre);
@@ -24,16 +24,36 @@ app.get('/api/genres/:id', (req, res) => {
 
 app.post('/api/genres', (req, res) => {
   const { error } = validateGenre(req.body);
-  if (error) {
-    //Break the Function don't continue
-    return console.log(error.details[0].message);
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const genre = {
     id: genres.length + 1,
     name: req.body.name,
   };
   genres.push(genre);
+  res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send('The genre with the given ID was not found.');
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send('The genre with the given ID was not found.');
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  console.log(genres);
   res.send(genre);
 });
 
