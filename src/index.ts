64784323af3c32c9af00df81767ec8a18601ed42
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-03-11 20:02:12 +0300

    ci: create post and get requests for the genres add Joi validation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log("ssmmmxlx")
+import express from 'express';
+import Joi from 'joi';
+
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+  { id: 1, name: 'Acton' },
+  { id: 2, name: 'Horror' },
+  { id: 3, name: 'Romance' },
+];
+
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send('The genre with the given ID was not found.');
+  res.send(genre);
+});
+
+app.post('/api/genres', (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) {
+    //Break the Function don't continue
+    return console.log(error.details[0].message);
+  }
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+function validateGenre(genre: { id: string | number; name: string }) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(genre);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}....`));
