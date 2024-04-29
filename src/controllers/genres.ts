be28f95399550cf258c4d55fe47f43a05559770f
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-04-29 19:31:12 +0300

    chore: create node

--- src/controllers/genres.ts
+++ src/controllers/genres.ts
@@ -1,7 +1,16 @@
 async (params: string) => {
   const string = "https://study-buddy-ai.vercel.app";
-  let x = 10
+  let x = 10;
   if (x === 3) {
   } else {
   }
 };
+
+class Node {
+  val: number;
+  next: null;
+  constructor(next: null, val: number) {
+    this.val = val;
+    this.next = next;
+  }
+}
